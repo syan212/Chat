084dd0bd904903e694647b1414378c55632909e3
syan212 <syan235711@gmail.com>
2025-01-12 20:01:33 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+const fs = require('lib/fs')
 function submit() {
   let uname = document.getElementById("uname").value;
   let pword = document.getElementById("pword").value;
+  fs.readFile('Input.txt', (err, data) => {
+    if (err) throw err;
+    let d = data;
+  });
   if (uname == "admin123" & pword == "password") {
     document.getElementById("web_page").style.display = "block";
     document.getElementById("login").style.display = "none";
+    alert(d);
   }
 }
