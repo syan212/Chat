c227c397b83c5e44d85e444baf9c241f10c37988
syan212 <syan235711@gmail.com>
2025-01-12 20:25:15 +0000

    Update script.j

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const fs = require('fs');
 function submit() {
   let uname = document.getElementById("uname").value;
   let pword = document.getElementById("pword").value;
   
-  fs.readFile('Source code/password.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    let d = data;
-
-    if (uname == "admin123" && pword == "password") {
-      document.getElementById("web_page").style.display = "block";
-      document.getElementById("login").style.display = "none";
-      alert(d);
-    }
-  });
+  fetch('Source code/password.txt')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.text();
+    })
+    .then(data => {
+      if (uname == "admin123" && pword == "password") {
+        document.getElementById("web_page").style.display = "block";
+        document.getElementById("login").style.display = "none";
+        alert(data);
+      }
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
 }
