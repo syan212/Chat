4e05c5a6736dc24c1e3de0ddd024d52c9d0095fc
syan212 <syan235711@gmail.com>
2025-01-12 20:22:12 +0000

    ahhhhhhhhhhhhhhh

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const fs = require('lib/fs')
+const fs = require('fs');
 function submit() {
   let uname = document.getElementById("uname").value;
   let pword = document.getElementById("pword").value;
-  fs.readFile('Source code/password.txt', (err, data) => {
+  
+  fs.readFile('Source code/password.txt', 'utf8', (err, data) => {
     if (err) throw err;
     let d = data;
+
+    if (uname == "admin123" && pword == "password") {
+      document.getElementById("web_page").style.display = "block";
+      document.getElementById("login").style.display = "none";
+      alert(d);
+    }
   });
-  if (uname == "admin123" && pword == "password") {
-    document.getElementById("web_page").style.display = "block";
-    document.getElementById("login").style.display = "none";
-    alert(d);
-  }
 }
